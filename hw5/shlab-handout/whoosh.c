33c3ff28938f58fb633d5c3309dfca2489e1d44a
Aaron <pabstaaron@gmail.com>
2017-03-24 18:47:27 -0600

    mad debugging session with kopta

--- hw5/shlab-handout/whoosh.c
+++ hw5/shlab-handout/whoosh.c
@@ -30,7 +30,7 @@ int main(int argc, char **argv) {
 }
 
 static void run_script(script *scr) {
-  printf("Num groups: %d", scr->num_groups);
+  //printf("Num groups: %d", scr->num_groups);
   if (scr->num_groups == 1) {
     run_group(&scr->groups[0]);
   } else {
@@ -42,26 +42,88 @@ static void run_script(script *scr) {
 static void run_group(script_group *group) {
   
   /* You'll have to make run_group do better than this, too */
-  if (group->repeats != 1)
-    fail("only repeat 1 supported");
+  /* if (group->repeats != 1) */
+  /*   fail("only repeat 1 supported"); */
   if (group->result_to != NULL) 
     fail("setting variables not supported");
 
-  if (group->num_commands == 1) {
-    run_command(&group->commands[0]);
-  } else {
-    /* And here */
-    fail("only 1 command supported");
-  }
+  /* if (group->num_commands == 1) { */
+  /*   run_command(&group->commands[0]); */
+  /* } else { */
+  /*   /\* And here *\/ */
+  /*   fail("only 1 command supported"); */
+  /* } */
 
-  if(group->mode == GROUP_SINGLE){
+  int** fd;
+  fd = (int**)malloc((group->repeats * group->num_commands)*sizeof(int*));
+  for(int i = 0; i < group->repeats * group->num_commands; i++){
+    fd[i] = malloc(2*sizeof(int));
+  } 
+  
+  if(group->mode == GROUP_AND){
     for(int j = 0; j < group->repeats; j++) // Fire off a set for each repeat of the grp
       for(int i = 0; i < group->num_commands; i++){
 	//Fire off a new process for each commmand
+	if(j == 0 && i == 0){
+	  // First process
+	  pipe(fd[0]);
+
+	  if(fork() == 0){
+	    printf("Redirecting stdout to %d\n", fd[0][1]);
+	    dup2(fd[0][1], 1);
+	    
+	    //close(fd[0][0]);
+	    //close(fd[0][1]);
+	    run_command(&group->commands[0]);
+
+	    
+	  }
+	}
+	else if(i == group->num_commands-1){
+	  // Last Proc
+	  if(fork() == 0){
+	    printf("Last: Redirecting stdin to %d\n", fd[j+i-1][0]);
+	    dup2(fd[j+i-1][0], 0);
+
+	    close(fd[j+i-1][1]);
+	    //close(fd[j+i-1][0]);
+	    run_command(&group->commands[i]);
+
+	  }
+	}
+	else{
+	  // Intermidiate proc
+	  pipe(fd[j+i]);
+
+	  if(fork() == 0){
+	    printf("Intermidiate: Redirecting stdin to %d\n", fd[j+i-1][0]);
+	    dup2(fd[j+i-1][0], 0); // Grab the tail end of the last pipe
+	    printf("Intermidiate: Redirecting stdout to %d\n", fd[j+i][1]);
+	    dup2(fd[j+i][1], 1); // Grab the head of the new pipe
+
+	    close(fd[j+i-1][1]);
+	    //close(fd[j+i][0]);
+	    //close(fd[j+i-1][0]);
+	    //close(fd[j+i][1]);
+	    run_command(&group->commands[i]);
+	  }
+	}
       }
   }
   else{
     fail("group_and and group_or not supported");
+  }
+
+  for(int i = 0; i < group->repeats * group->num_commands-1; i++){
+    //close(fd[i][0]);
+    printf("Closing %d\n", fd[i][1]);
+    close(fd[i][1]);
+  }
+
+  int status;
+  int i;
+  for(i = 0; i < group->repeats * group->num_commands; i++){
+    wait(&status);
   }
 }
 
