9af40c8b3e9e544b40f9e989a08cb4b95a70d88c
Aaron <pabstaaron@gmail.com>
2017-04-25 12:52:51 -0600

    Got hw7 to ~90% completion

--- hw7/servlab-handout/tinychat.c
+++ hw7/servlab-handout/tinychat.c
@@ -12,7 +12,10 @@
 #include "dictionary.h"
 #include "more_string.h"
 
-void doit(int fd);
+#define SBUF_SIZE 10
+#define NUM_THREADS 4
+
+void doit(void* fd);
 dictionary_t *read_requesthdrs(rio_t *rp);
 void read_postquery(rio_t *rp, dictionary_t *headers, dictionary_t *d);
 void parse_query(const char *uri, dictionary_t *d);
@@ -22,11 +25,14 @@ void clienterror(int fd, char *cause, char *errnum,
 static void print_stringdictionary(dictionary_t *d);
 void serve_login(int fd, dictionary_t* query);
 void appendToConvo(char* topic, char* user, char* content);
-void serve_convo(fd, query);
-void grab_content(fd, query);
-void serve_import(fd, query);
-char* readConvo_noHtml(char* topic)
-
+void serve_convo(int fd, dictionary_t* query);
+void grab_content(int fd, dictionary_t* query);
+void serve_import(int fd, dictionary_t* query);
+char* readConvo_noHtml(char* topic);
+void spin_thread(int fd);
+void serve_convo_import(int fd, dictionary_t* query);
+void *handle_threads(void *na);
+  
 dictionary_t* convos; // Each value of convos will be a msg_wrap
 
 typedef struct{
@@ -39,7 +45,33 @@ typedef struct{
   void* msgs;
 }msg_wrapper;
 
+// Data structure used to implemenent a locking mechanism on the convo dict
+typedef struct{
+  sem_t sem;
+  dictionary_t* convos;
+}convo_dict;
+
+typedef struct {
+  int *buf; /* Buffer array */
+  int n; /* Maximum number of slots */
+  int front; /* buf[(front+1)%n] is first item */
+  int rear; /* buf[rear%n] is last item */
+  sem_t mutex; /* Protects accesses to buf */
+  sem_t slots; /* Counts available slots */
+  sem_t items; /* Counts available items */
+} sbuf_t;
+
 char* readConvo(char* topic);
+void sbuf_insert(sbuf_t *sp, int item);
+void sbuf_init(sbuf_t *sp, int n);
+int sbuf_remove(sbuf_t *sp);
+
+//convo_dict* make_convo_dict()
+
+sem_t sema;
+sbuf_t connfds;
+
+//convo_dict* convos;
 
 int main(int argc, char **argv)
 {
@@ -47,7 +79,11 @@ int main(int argc, char **argv)
   char hostname[MAXLINE], port[MAXLINE];
   socklen_t clientlen;
   struct sockaddr_storage clientaddr;
-
+  
+  Sem_init(&sema, 0, 1);
+  sbuf_init(&connfds, SBUF_SIZE);
+  
+  //convos = make_convo_dict();
   convos = make_dictionary(COMPARE_CASE_SENS, free);
   
   /* Check command line args */
@@ -66,14 +102,22 @@ int main(int argc, char **argv)
   /* Also, don't stop on broken connections: */
   Signal(SIGPIPE, SIG_IGN);
 
-  /* appendToConvo("Kylie's hot", "aaron", "Seriously though"); */
-  /* printf("%s\n", readConvo("Kylie's hot")); */
-  /* appendToConvo("Kylie's hot", "aaron", "I like her a lot"); */
-  /* printf("%s\n", readConvo("Kylie's hot")); */
-  /* appendToConvo("Kylie's hot", "aaron", "I like her a lot"); */
-  /* printf("%s\n", readConvo("Kylie's hot")); */
-  /* appendToConvo("Kylie's hot", "aaron", "I like her a lot"); */
-  /* printf("%s\n", readConvo("Kylie's hot")); */
+  appendToConvo("Kylie", "aaron", "Seriously though");
+  printf("%s\n", readConvo("Kylie"));
+  appendToConvo("Kylie hot", "aaron", "I like her a lot");
+  printf("%s\n", readConvo("Kylie"));
+  appendToConvo("Kylie", "aaron", "I like her a lot");
+  printf("%s\n", readConvo("Kylie"));
+  appendToConvo("Kylie", "aaron", "I like her a lot");
+  printf("%s\n", readConvo("Kylie"));
+
+  // Set up threads
+  pthread_t th;
+  int i;
+  for (i = 0; i < NUM_THREADS; i++) {
+    Pthread_create(&th, NULL, handle_threads, NULL);
+    Pthread_detach(th);
+  }
   
   while (1) {
     clientlen = sizeof(clientaddr);
@@ -81,19 +125,89 @@ int main(int argc, char **argv)
     if (connfd >= 0) {
       Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE,
                   port, MAXLINE, 0);
-      printf("Accepted connection from (%s, %s)\n", hostname, port);
-      doit(connfd);
-      Close(connfd);
+      //printf("Accepted connection from (%s, %s)\n", hostname, port);
+      // We'll want to spin off a new thread here instead of calling doit
+      //spin_thread(connfd);
+      //doit(connfd);
+      //Close(connfd);
+
+      sbuf_insert(&connfds, connfd);
     }
   }
+}
+
+convo_dict* make_convo_dict(){
+  convo_dict* cd = malloc(sizeof(convo_dict));
+  cd->convos = make_dictionary(COMPARE_CASE_SENS, free);
+  Sem_init(&cd->sem, 0, 1);
+  return cd;
+}
+
+// Locks up the dictionary and returns the desired conversation
+msg_wrapper* get_msg_wrapper(convo_dict* cd, char* topic){
+  P(&cd->sem);
+  msg_wrapper* wrap = (msg_wrapper*)dictionary_get(cd->convos, topic);
+}
+
+void unlock_dict(convo_dict* cd){
+  V(&cd->sem);
+}
+
+void sbuf_init(sbuf_t *sp, int n) {
+  sp->buf = Calloc(n, sizeof(int));
+  sp->n = n; /* max of n items */
+  sp->front = sp->rear = 0; /* empty iff front == rear */
+  Sem_init(&sp->mutex, 0, 1); /* for locking */
+  Sem_init(&sp->slots, 0, n); /* initially n empty slots */
+  Sem_init(&sp->items, 0, 0); /* initially zero data items */
+}
+
+void sbuf_insert(sbuf_t *sp, int item) {
+  P(&sp->slots); /* wait for available slot */
+  P(&sp->mutex); /* lock */
+  sp->buf[(++sp->rear)%(sp->n)] = item;
+  V(&sp->mutex); /* unlock */
+  V(&sp->items); /* announce available item */
+}
+
+int sbuf_remove(sbuf_t *sp) {
+  int item;
+  P(&sp->items); /* wait for available item */
+  P(&sp->mutex); /* lock */
+  item = sp->buf[(++sp->front)%(sp->n)];
+  V(&sp->mutex); /* unlock */
+  V(&sp->slots); /* announce available slot */
+  return item;
+}
+
+void spin_thread(int fd){
+  int* fd_p = malloc(sizeof(int));
+  *fd_p = fd;
+
+  pthread_t th;
+  Pthread_create(&th, NULL, doit, fd_p); // Create a thread running doit
+  Pthread_detach(th); // We don't care about the result, so we don't need to wait on the thread
+}
+
+void *handle_threads(void *na){
+  int connfd;
+  while(1){
+    connfd = sbuf_remove(&connfds);
+
+    doit((void*) &connfd);
+  }
+
+  return NULL;
 }
 
 /*
  * doit - handle one HTTP request/response transaction
  */
-void doit(int fd)
+void doit(void* fd_p)
 {
-    char buf[MAXLINE], *method, *uri, *version;
+  int fd = *((int*)fd_p);
+  
+  char buf[MAXLINE], *method, *uri, *version;
   rio_t rio;
   dictionary_t *headers, *query;
 
@@ -101,15 +215,15 @@ void doit(int fd)
   Rio_readinitb(&rio, fd);
   if (Rio_readlineb(&rio, buf, MAXLINE) <= 0)
     return;
-  printf("%s", buf);
+  //printf("%s", buf);
  
   if (!parse_request_line(buf, &method, &uri, &version)) {
-    printf("URI: %s\n", uri);
+    // printf("URI: %s\n", uri);
     clienterror(fd, method, "400", "Bad Request",
                 "TinyChat did not recognize the request");
   } else {
     if (strcasecmp(version, "HTTP/1.0")
-        && strcasecmp(version, "HTTP/1.1")) {
+        && strcasecmp(version, "HTTP/1.1")) { 
       clienterror(fd, version, "501", "Not Implemented",
                   "TinyChat does not implement that version");
     } else if (strcasecmp(method, "GET")
@@ -124,15 +238,19 @@ void doit(int fd)
       parse_uriquery(uri, query);
       if (!strcasecmp(method, "POST")){
 	char* content = dictionary_get(query, "content");
-	printf("content is %s\n", content);
+	//printf("content is %s\n", content);
         read_postquery(&rio, headers, query);
       }
       /* For debugging, print the dictionary */
-      print_stringdictionary(query);
+      //print_stringdictionary(query);
 
       
       // Will need to add some additional logic here to service
-      if(starts_with("/conversation", uri)){
+      if(starts_with("/conversationimport", uri)){
+	// Send the conversation specified in the url as plain
+	serve_convo_import(fd, query);
+      }
+      else if(starts_with("/conversation", uri)){
 	// Send the conversation specified in the url as plain
 	serve_convo(fd, query);
       }
@@ -159,6 +277,8 @@ void doit(int fd)
     free(uri);
     free(version);
   }
+  Close(fd);
+  //return NULL;
 }
  
 /*
@@ -215,16 +335,156 @@ static char *ok_header(size_t len, const char *content_type) {
   return header;
 }
 
+// Takes a message line containing the username and message content
+// and returns a string array containing the username and message content
+char** extractMessage(char* msg){
+  //printf("Unextracted msg: %s\n", msg);
+  
+  char* msg2 = strdup(msg);
+
+  int tokens_alloc = 1;
+  int tokens_used = 0;
+
+  char** tokens = calloc(tokens_alloc, sizeof(char*));
+  char* token;
+  char* strtok_cntx;
+  char* delim = ":"; // string we want to split around
+  
+  for(token = strtok_r(msg, ": ", &strtok_cntx); token != NULL; token = strtok_r(NULL, delim, &strtok_cntx)){
+    if(tokens_used == tokens_alloc){
+      tokens_alloc *= 2;
+      tokens = realloc(tokens, tokens_alloc * sizeof(char*));
+    }
+    tokens[tokens_used++] = strdup(token);
+  }
+
+  if(tokens_used == 0){
+    free(tokens);
+    tokens = NULL;
+  }
+  else
+    tokens = realloc(tokens, tokens_used * sizeof(char*));
+
+  free(msg2);
+
+  // At this point, there will be an extra space at the beginning of content
+  int origlen = strlen(tokens[1]);
+  char* content2 = malloc(origlen); // seg fault occuring here
+  int i;
+  for(i = 0; i < origlen-1; i++){
+    content2[i] = tokens[1][i+1];
+  }
+  content2[origlen-1] = '\0';
+  tokens[1] = content2;
+  
+  return tokens;
+}
+
+/*
+ * Imports a conversation from another chat server
+ * TODO - Picking up an extra newline somewhere
+ */
+void serve_import(int fd, dictionary_t* query){
+  // Need to fire up a connection to another server, make a conversation request
+  // And then add that data into our own conversation data structure
+
+  // Pull out needed information
+  char* topic = dictionary_get(query, "topic");
+  char* host = dictionary_get(query, "host");
+  char* port = dictionary_get(query, "port");
+    
+  int cFd = Open_clientfd(host, port);
+  
+  char* req = append_strings("GET /conversationimport?topic=",
+			     topic,
+			     " HTTP/1.0\r\n\r\n",
+			     NULL);
+
+  // Send HTTP request
+  Rio_writen(cFd, req, strlen(req)); // Will this send the right URL?
+
+  // Need to read incoming transmission line by line
+  rio_t rio;
+  char buf[MAXLINE];
+  int len;
+  rio_readinitb(&rio, cFd);
+  while((len = rio_readlineb(&rio, buf, MAXLINE)) != 0){
+    // Need to ignore HTTP 
+    //printf("%s", buf);
+    char** msg = extractMessage(buf);
+    //printf("-Extracted message-\n");
+    //printf("user: %s\n", msg[0]); 
+    //printf("content: %s\n", msg[1]);
+    appendToConvo(topic, strdup(msg[0]), strdup(msg[1]));
+    free(msg[0]);
+    free(msg[1]);
+    free(msg);
+  }
+  
+  char* header = ok_header(0, "text/html; charset=utf-8");
+  Rio_writen(fd, header, strlen(header));
+  free(header); 
+}
+
+/* 
+ * Adds the content specified in query to the conversation specified in query
+ */
+void grab_content(int fd, dictionary_t* query){
+  char* topic = dictionary_get(query, "topic");
+  char* user = dictionary_get(query, "user");
+  char* content = dictionary_get(query, "content"); 
+
+  appendToConvo(topic, user, content);
+
+  char* header = ok_header(0, "text/html; charset=utf-8");
+  Rio_writen(fd, header, strlen(header));
+  free(header); 
+}
+
 /*
  * Sends the content of the of the conversation specified
  * in the query's topic field as plain text to the client
  */
-void serve_convo(fd, query){
+void serve_convo(int fd, dictionary_t* query){
   // Step 1: Get conversation data
+  //printf("In regular version\n");
+  
   char* topic = dictionary_get(query, "topic");
   char* convoText = readConvo_noHtml(topic);
 
+  //printf("Sending conversation: %s\nContent: %s\n", topic, convoText);
+  
   // Step 2: Send that data back to the client
+  // TODO - This might need to be an HTTP response
+
+  char* header = ok_header(strlen(convoText), "text/html; charset=utf-8");
+  Rio_writen(fd, header, strlen(header));
+  free(header);
+  
+  Rio_writen(fd, convoText, strlen(convoText));
+
+  if(strlen(convoText) != 0 && convoText != NULL)
+    free(convoText);
+}
+
+void serve_convo_import(int fd, dictionary_t* query){
+  // Step 1: Get conversation data
+  //printf("In import version\n");
+  char* topic = dictionary_get(query, "topic");
+  char* convoText = readConvo_noHtml(topic);
+
+  //printf("Sending conversation: %s\nContent: %s\n", topic, convoText);
+  
+  // Step 2: Send that data back to the client
+  // TODO - This might need to be an HTTP response
+
+  /* char* header = ok_header(strlen(convoText), "text/html; charset=utf-8"); */
+  /* Rio_writen(fd, header, strlen(header)); */
+  /* free(header); */
+  
+  Rio_writen(fd, convoText, strlen(convoText));
+
+  //free(convoText);
 }
 
 /*
@@ -247,7 +507,7 @@ void serve_form(int fd, dictionary_t* query)
    // Add message to the dictionary
   appendToConvo(topic, user, content);
   char* convoText = readConvo(topic);
-  printf("%s\n", convoText);
+  //printf("%s\n", convoText);
   
   body = append_strings("<html><body>\r\n",
                         "<p>Welcome to TinyChat, ",
@@ -272,18 +532,18 @@ void serve_form(int fd, dictionary_t* query)
   /* Send response headers to client */
   header = ok_header(len, "text/html; charset=utf-8");
   Rio_writen(fd, header, strlen(header));
-  printf("Response headers:\n");
-  printf("%s", header);
+  //printf("Response headers:\n");
+  //printf("%s", header);
 
   /* printf("Before\n"); */
-  free(header); // Seg fault here
+  free(header); 
   /* printf("After\n"); */
 
   /* Send response body to client */
   Rio_writen(fd, body, len);
 
  
-  
+  free(convoText);
   free(body);
 }
 
@@ -292,21 +552,24 @@ void serve_form(int fd, dictionary_t* query)
 // TODO: Need to copy content into a different memory location that won't be freed by doit
 void appendToConvo(char* topic, char* user, char* content){
   if(content == NULL || !strcmp(content, "")){
-    printf("Returning early\n");
+    //printf("Returning early\n");
     return;
   }
-  char* contentTwo = malloc(strlen(content));
-  char* userTwo = malloc(strlen(user));
+  char* contentTwo = malloc((strlen(content)+1) * sizeof(char));
+  char* userTwo = malloc((strlen(user)+1) * sizeof(char));
   strcpy(contentTwo, content);
   strcpy(userTwo, user);
-  printf("Content in append: %s\n", contentTwo);
+  //printf("Content in append: %s\n", contentTwo);
+  P(&sema); // Install a lock
   msg_wrapper* wrap = (msg_wrapper*)dictionary_get(convos, topic);
-  if(wrap == NULL){
-    printf("Creating a new conversation\n");
+  if(wrap == NULL){ 
+    //printf("Creating a new conversation\n");
+    //P(&sema); // Install a lock
     wrap = calloc(1, sizeof(msg_wrapper));
     wrap->len = 0;
     wrap->msgs = NULL;
     dictionary_set(convos, topic, (void*)wrap);
+    //V(&sema); // Unlock
   }
 
   msg* oldMsgs = (msg*)wrap->msgs;
@@ -316,28 +579,33 @@ void appendToConvo(char* topic, char* user, char* content){
   int i;
   for(i = 0; i < wrap->len; i++){
     newMsgs[i] = oldMsgs[i];
-    printf("%s\n", oldMsgs[i].content);
+    //printf("%s\n", oldMsgs[i].content);
   }
 
   msg* newMsg = (msg*)calloc(1, sizeof(msg));
   newMsg->user = userTwo;
-  newMsg->content = contentTwo;
+  newMsg->content = contentTwo; 
   
   newMsgs[wrap->len] = *newMsg;
 
   //int oldLength = wrap->len;
+  //P(&sema); // Install a lock
   wrap->len = wrap->len + 1;
-  wrap->msgs = (void*)newMsgs;
+  wrap->msgs = (void*)newMsgs; 
+  //V(&sema); // Unlock
   // printf("Print me! %d\n", oldLength);
   //printf("Added new message! User: %s \tContent: %s\n", newMsgs[oldLength].user, newMsg[oldLength].content);
-  printf("Bailing out of append\n");
+  //printf("Bailing out of append\n");
 
   free(oldMsgs);
+  V(&sema); // Unlock
+  // End Critical Region
 }
 
 // Reads the conversation of the specified topic into an array of messages of length len// Returns NULL if the conversation does not exist
 char* readConvo(char* topic){
-  printf("Reading convo\n");
+  //printf("Reading convo\n");
+  //P(&sema);
   msg_wrapper* wrap = (msg_wrapper*)dictionary_get(convos, topic); 
   if(wrap == NULL)
     return ""; 
@@ -355,7 +623,7 @@ char* readConvo(char* topic){
   }
 
   // Build in the new html
-  char* html = calloc(len, sizeof(char));
+  char* html = calloc(len+1, sizeof(char));
   for(i = 0; i < wrap->len; i++){
     msg curr = ((msg*)(wrap->msgs))[i];
     html = strcat(html, "<p>");
@@ -365,11 +633,13 @@ char* readConvo(char* topic){
     html = strcat(html, "</p>");
   }
 
+  //V(&sema);
   return html;
 }
 
 char* readConvo_noHtml(char* topic){
-printf("Reading convo\n");
+  //printf("Reading convo\n");
+  //P(&sema);
   msg_wrapper* wrap = (msg_wrapper*)dictionary_get(convos, topic); 
   if(wrap == NULL)
     return ""; 
@@ -381,22 +651,22 @@ printf("Reading convo\n");
     msg curr = ((msg*)(wrap->msgs))[i];
     len += strlen(curr.user);
     len += strlen(curr.content);
-    len += 3; 
+    len += 4; 
   }
 
   // Build in the new html
-  char* html = calloc(len, sizeof(char));
+  char* html = calloc(len + 4, sizeof(char));
   for(i = 0; i < wrap->len; i++){
     msg curr = ((msg*)(wrap->msgs))[i];
     html = strcat(html, curr.user);
     html = strcat(html, ": ");
     html = strcat(html, curr.content);
-    html = strcat(html, "\n"); // Carriage return
+    html = strcat(html, "\r\n"); // Carriage return
   }
-
+  //V(&sema);
   return html;
 }
-
+  
 /*
  * serve_login - sends a form to a client
  */
@@ -428,8 +698,8 @@ void serve_login(int fd, dictionary_t* query)
   /* Send response headers to client */
   header = ok_header(len, "text/html; charset=utf-8");
   Rio_writen(fd, header, strlen(header));
-  printf("Response headers:\n");
-  printf("%s", header);
+  //printf("Response headers:\n");
+  //printf("%s", header);
 
   free(header);
 
